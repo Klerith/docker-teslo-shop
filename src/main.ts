a4ae64b079a4c82039524a5487715f8d5e5d7520
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-09 11:46:31 -0400

    CORS Enabled

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true});
   const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');
